ee28a024ea4e82af050669a3908622bf052a9b89
eddcp <eduardocspv@gmail.com>
2021-12-23 21:54:05 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -15,6 +15,8 @@ public class User implements Serializable {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
